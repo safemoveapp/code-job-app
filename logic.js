7ff2cbfc0564cf02d50c98fef51e0e6a0da2babb
Ren <rjingsiu@gmail.com>
2017-11-14 21:43:51 -0800

    render map when clicking links on card

--- logic.js
+++ logic.js
@@ -1,9 +1,7 @@
 jQuery(function ($) {
     var job = "javascript";
     //var city = "chicago,il";
-    var location;
-    
-    var company = "kpmg";
+  
     var width = "600";
     var height = "450";
 
@@ -12,58 +10,81 @@ jQuery(function ($) {
     var ikey = "AIzaSyD8yKqnL-7Y8-2cOWW_3YqN66bePhCWp6g";
     var gSrc = "https://www.google.com/maps/embed/v1/place?key=";
 
-    var test = "";
+    
 
 
 
     function displayCards() {
         var city = window.localStorage.getItem('input');
         var queryUrl3 = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?text=" +
-        "&city=" + city;
+            "&city=" + city;
         $.ajax({
             url: queryUrl3,
             method: 'GET'
         }).done(function (response) {
             console.log(response);
             var list = response.resultItemList;
-            // for (var i = 0; i < list.length; i++) {
-            //     // console.log(list[i].jobTitle);
-            //     // console.log(list[i].location);
-            //     // console.log(list[i].company);
-            //     // console.log(list[i].detailUrl);
-            //     console.log("-----------------");
-            //     location = list[0].location;
-            //     console.log(location);
-            // }
-            // var jobDiv = $("<div class='carousel'>");
-       // var jobDiv = $("<div class='card - size card purple darken - 1'>  <div class='card - content white - text'>'");
-       for (i = 0; i < 10; i++) {
-        var jobDiv = $(".carousel");
-        jobDiv.append("<div class='card-size card purple darken-1'>  <div class='card-content white-text'>" + "<div class='card-title job-name'>" +
-            (list[i].jobTitle).substring(0, 30) + " </div> <div class='card-title city-name'>" + 
-            list[i].location + " </div> <div class='card-title posted-date'>" +
-            list[i].date + "</div> <div class='card-title pcompany-name' id='" + i + "'>" + list[i].company + "</div> <div class='card-title job-link'> See map for location..");
-         // above sample line replaces job posting detail with map  
-        // response.resultItemList[i].detailUrl + "' > See map for location.. </a>");
-       }
-            // var $iframe = $("<iframe>");
-            // $iframe.attr("width", width);
-            // $iframe.attr("height", height);
-            // $iframe.attr("frameborder", "0");
-            // $iframe.attr("style", "border:0");
-            // $iframe.attr("src", gSrc + ikey + "&q=" + company + ", " + newCity);
-            // $("#map").append($iframe);
 
+            for (i = 0; i < 8; i++) {
+                var $jobDiv = $(".carousel");
+                // $jobDiv.append("<div class='card-size card purple darken-1'><div class='card-content white-text'>" + "<div class='card-title job-name'>" +
+                //     (list[i].jobTitle).substring(0, 30) + " </div><div class='card-title city-name' data-name=" + list[i].location + ">" +
+                //     list[i].location + " </div> <div class='card-title posted-date'>" +
+                //     list[i].date + "</div> <div class='card-title pcompany-name' data-name=" + list[i].company + ">" +
+                //     list[i].company + "</div> <a href='#' class='map-link'>See map for location</a>");
+                // above sample line replaces job posting detail with map  
 
-            // var plus = "+";
-            // var newCity = location.split(",").join(plus);
-            // console.log(newCity);
-       
+               var $card = $("<div>").addClass("card-size card purple darken-1");
+               var $content = $("<div>").addClass("card-content white-black");
+               var $jobTitle = $("<div>").addClass("card-title job-name");
+                    $jobTitle.append((list[i].jobTitle).substring(0, 20));
+               var $location = $("<div>").addClass("card-title city-name");
+                    $location.append(list[i].location);
+               var $date = $("<div>").addClass("card-title posted-date");
+                    $date.append(list[i].date)
+               var $company = $("<div>").addClass("card-title company-name");
+                    $company.append(list[i].company);
+               var $link = $("<a>").addClass("map-link");
+                    $link.attr("data-company", list[i].company);
+                    $link.attr("data-location", list[i].location);
+                    $link.append("See map on link");
+               
+               $content.append($jobTitle);
+               $content.append($location);
+               $content.append($date);
+               $content.append($company);
+               $content.append($link);
+
+               $card.append($content);
+
+               $jobDiv.append($card);
+            }
+            // test = $(".company-name").text(); 
+            // console.log(test);
         });// end of ajax call 
 
         //console.log(test);
-        console.log(city);
+       
     }
-    
+
+    function renderMap() {
+        var $addr = $(this).data("company").replace(/[ ,]+/g, "+");
+        var $city = $(this).data("location").replace(/[ ,]+/g, "+");
+        var $iframe = $("<iframe>");
+        $iframe.attr("width", width);
+        $iframe.attr("height", height);
+        $iframe.attr("frameborder", "0");
+        $iframe.attr("style", "border:0");
+        $iframe.attr("src", gSrc + ikey + "&q=" + $addr + ", " + $city);
+        $("#map").html($iframe);
+
+
+        
+        
+    }
+
+
     displayCards();
+
+    $(document).on("click", ".map-link", renderMap);
 })
